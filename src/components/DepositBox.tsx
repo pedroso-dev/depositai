d36d6dc5246c63ab3ca8cef14682974de4c701f1
Mateus Pedroso <mateuspedroso.dev@gmail.com>
2025-08-29 23:49:50 -0300

    feat: create DepositBox component and define static size of deposits

---
+++ src/components/DepositBox.tsx
@@ -0,0 +1,26 @@
+type DepositBoxProps = {
+  value: number;
+  isCompleted: boolean;
+  onClick: (value: number) => void;
+};
+
+export default function DepositBox({
+  value,
+  isCompleted,
+  onClick,
+}: DepositBoxProps) {
+  const baseStyle =
+    "flex h-16 w-16 cursor-pointer items-center justify-center  rounded-md border-2 text-lg font-bold transition-all";
+  const completedStyle = "border-green-500 bg-green-500 text-white";
+  const pendingStyle =
+    "border-gray-400 bg-gray-700 text-white hover:border-green-400 hover:bg-gray-600";
+
+  return (
+    <div
+      className={`${baseStyle} ${isCompleted ? completedStyle : pendingStyle}`}
+      onClick={() => onClick(value)}
+    >
+      {value}
+    </div>
+  );
+}
