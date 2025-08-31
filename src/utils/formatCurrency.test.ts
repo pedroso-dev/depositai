36bf5ad6f73cace79acc7af54620596e6be898d0
Mateus Pedroso <mateuspedroso.dev@gmail.com>
2025-08-31 12:30:09 -0300

    feat: add more tests for formatCurrency

--- src/utils/formatCurrency.test.ts
+++ src/utils/formatCurrency.test.ts
@@ -12,4 +12,36 @@ describe("formatCurrency", () => {
     const result = formatCurrency(value);
     expect(result).toContain("R$ 0,00");
   });
+
+  it("should handle negative values", () => {
+    const value = -1234.56;
+    const result = formatCurrency(value);
+    expect(result).toContain("-R$ 1.234,56");
+  });
+
+  it("should handle large values", () => {
+    const value = 1234567890.12;
+    const result = formatCurrency(value);
+    expect(result).toContain("R$ 1.234.567.890,12");
+  });
+
+  it("should round numbers with more than two decimal places correctly", () => {
+    const valueBigger = 123.456;
+    const resultBigger = formatCurrency(valueBigger);
+    expect(resultBigger).toContain("R$ 123,46");
+    const valueSmaller = 99.994;
+    const resultSmaller = formatCurrency(valueSmaller);
+    expect(resultSmaller).toContain("R$ 99,99");
+  });
+
+  it("shoul pad numbers with one decimal place", () => {
+    const value = 123.4;
+    const result = formatCurrency(value);
+    expect(result).toContain("R$ 123,40");
+  });
+
+  it("shold handle NaN", () => {
+    const result = formatCurrency(NaN);
+    expect(result).toContain("NaN");
+  });
 });
