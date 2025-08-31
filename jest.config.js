43b807ef79700d265591f2693e1f6e12c2773550
Mateus Pedroso <mateuspedroso.dev@gmail.com>
2025-08-30 21:40:13 -0300

    feat configure Jest for testing

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+// jest.config.js
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
+  preset: "ts-jest",
+};
+
+module.exports = createJestConfig(customJestConfig);
